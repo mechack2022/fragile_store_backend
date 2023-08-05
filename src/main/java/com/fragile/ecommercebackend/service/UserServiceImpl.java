48205902cae40f7751d271fe42e72aab62a53033
mechack2022 <dtoms624@gmail.com>
2023-08-05 08:21:05 +0100

    product service implemented

--- src/main/java/com/fragile/ecommercebackend/service/UserServiceImpl.java
+++ src/main/java/com/fragile/ecommercebackend/service/UserServiceImpl.java
@@ -1,21 +1,37 @@
 package com.fragile.ecommercebackend.service;
 
+import com.fragile.ecommercebackend.config.JwtProvider;
 import com.fragile.ecommercebackend.exceptions.UserException;
 import com.fragile.ecommercebackend.model.User;
+import com.fragile.ecommercebackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
     @Override
     public User findUserById(Long userId) throws UserException {
-        return null;
+        Optional<User>  foundUser = userRepository.findById(userId);
+        if(foundUser.isPresent()){
+            return foundUser.get();
+        }
+        throw new UserException("User not found with id : " + userId);
     }
 
     @Override
     public User findUserProfileByJwt(String jwt) throws UserException {
-        return null;
+        String userEmail = jwtProvider.getEmailFromToken(jwt);
+
+        User user = userRepository.findByEmail(userEmail);
+        if(user == null){
+            throw new UserException("User not found");
+        }
+        return user;
     }
 }
