c7825285ba8fba12974482897575f9fe2e90fa5a
mechack2022 <dtoms624@gmail.com>
2023-08-29 12:30:24 +0100

    service package added

---
+++ src/test/java/com/fragile/ecommercebackend/service/CartItemServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.fragile.ecommercebackend.service;
+
+import com.fragile.ecommercebackend.repository.CartRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartItemServiceImplTest {
+ @Mock
+ private CartRepository cartRepository;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createCartItem() {
+    }
+
+    @Test
+    void updateCartItem() {
+    }
+
+    @Test
+    void isCartItemExist() {
+    }
+
+    @Test
+    void removeCartItem() {
+    }
+
+    @Test
+    void findCartItemById() {
+    }
+}
