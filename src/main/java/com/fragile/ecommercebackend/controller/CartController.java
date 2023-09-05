e3c37197c4933360c5f5c869bfedf9953d6145df
mechack2022 <dtoms624@gmail.com>
2023-09-05 17:03:34 +0100

    cart item deleted controller added

--- src/main/java/com/fragile/ecommercebackend/controller/CartController.java
+++ src/main/java/com/fragile/ecommercebackend/controller/CartController.java
@@ -1,14 +1,17 @@
 package com.fragile.ecommercebackend.controller;
 
+import com.fragile.ecommercebackend.exceptions.CartItemExeption;
 import com.fragile.ecommercebackend.exceptions.UserException;
 import com.fragile.ecommercebackend.model.Cart;
 import com.fragile.ecommercebackend.model.User;
 import com.fragile.ecommercebackend.request.AddItemToCatReq;
 import com.fragile.ecommercebackend.response.ApiResponse;
+import com.fragile.ecommercebackend.service.CartItemService;
 import com.fragile.ecommercebackend.service.CartService;
 import com.fragile.ecommercebackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.bridge.IMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,17 +25,20 @@ public class CartController {
     private final UserService userService;
 
     private final CartService cartService;
+
+    private CartItemService cartItemService;
+
     @GetMapping("/")
 //    @Operation(description="find cart by userId")
     public ResponseEntity<Cart> findUserCart(@RequestHeader("Authorization") String jwt) throws UserException {
         User user = userService.findUserProfileByJwt(jwt);
-       Cart cart = cartService.findUserCart(user.getId());
+        Cart cart = cartService.findUserCart(user.getId());
         log.info("user Id :{}", user.getId());
-    return new ResponseEntity<>(cart, HttpStatus.OK);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<ApiResponse> addItemToCart(@RequestHeader("Authorization") String jwt, @RequestBody AddItemToCatReq req){
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestHeader("Authorization") String jwt, @RequestBody AddItemToCatReq req) {
         User user = userService.findUserProfileByJwt(jwt);
         String message = cartService.addCartItem(user.getId(), req);
         ApiResponse apiResponse = new ApiResponse();
@@ -40,6 +46,17 @@ public class CartController {
         apiResponse.setMessage(message);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
 
+    }
+
+    @DeleteMapping("/{cartItemId}")
+    public ResponseEntity<ApiResponse> deletedCartItemHandler(@RequestHeader("Authorization") String jwt, @PathVariable("cartItemId") Long cartItemId)
+            throws CartItemExeption, UserException {
+        User user = userService.findUserProfileByJwt(jwt);
+        String message = cartItemService.removeCartItem(user.getId(), cartItemId);
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setStatus(true);
+        apiResponse.setMessage(message);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
 
